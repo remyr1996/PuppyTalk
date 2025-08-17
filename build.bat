94acb97a6cb95cb45fcd2e9c55e22fa1a62ea073
remyr1996 <remyr1996@gmail.com>
2025-08-17 15:54:30 -0400

    Updated build script

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo ========================================================================================
 @echo Prerequisites: 
 @echo - Need to have the 7Zip directory added to the PATH environment variable of Windows
-@echo - The Bambi Dumbtalk Script.exe file need to have already been compliled with Autohotkey
+@echo - The PuppyTalk Script.exe file need to have already been compliled with Autohotkey
+@echo - OR the Ahk2Exe utility must be in the PATH environment variable of Windows
 @echo ========================================================================================
 @echo.
 
@@ -13,6 +14,8 @@
 @set "PROJECT_DIR=%cd%"
 @cd "%PROJECT_DIR%"
 
+@Ahk2Exe.exe /in "%PROJECT_DIR%\PuppyTalk Script.ahk" /out "%PROJECT_DIR%\%packageName%.exe" /icon "%PROJECT_DIR%\Resources\img\Dumbtalk-Script-Icon-Running.ico"
+
 :: Reset the Build folder
 if exist "Build\" @rd /S /Q "Build"
 if not exist "Build\" @mkdir "Build"
@@ -22,7 +25,7 @@ if not exist "Build\" @mkdir "Build"
 
 :: Copy the necessary files to the package folder
 @robocopy "%PROJECT_DIR%" "Build\%packageName%" "README.md"
-@robocopy "%PROJECT_DIR%" "Build\%packageName%" "Puppy talk Script.exe"
+@robocopy "%PROJECT_DIR%" "Build\%packageName%" "%packageName%.exe"
 @robocopy "%PROJECT_DIR%" "Build\%packageName%" "settings.ini"
 
 :: Zip the package
